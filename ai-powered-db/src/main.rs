097112eef3202fc3a0af9421ecccb2e0236b7dd3
Guimarret <guimarret@gmail.com>
2025-06-13 23:26:17 -0300

    feat: buffer
    #TODO add compression

--- ai-powered-db/src/main.rs
+++ ai-powered-db/src/main.rs
@@ -20,3 +20,29 @@ async fn main(){
 
     publish(vec![msg], log_insert).await;
 }
+
+#[derive(Debug)]
+struct Message {
+        // timestamp: i64,
+        key : String,
+        value: String
+        // process_data: i64
+}
+
+async fn publish(mut msg: Vec<Message>, mut log: tokio::fs::File)-> Result<(), std::io::Error>{
+    let mut buffer: Vec<u8> = Vec::new();
+    for x in msg {
+        std::io::Write::write_all(&mut buffer, &[x.key.len() as u8])?;
+        std::io::Write::write_all(&mut buffer, x.key.as_bytes())?;
+        std::io::Write::write_all(&mut buffer, &(x.value.len() as u16).to_le_bytes())?;
+        std::io::Write::write_all(&mut buffer, x.value.as_bytes())?;
+    };
+    let checksum = crc32fast::hash(&buffer);
+    std::io::Write::write_all(&mut buffer, &checksum.to_le_bytes())?;
+    
+
+    log.write_all(&buffer).await.unwrap();
+    return Ok(());
+}
+    // #TODO Durability
+
