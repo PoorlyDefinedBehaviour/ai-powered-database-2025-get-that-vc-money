3a430e8f6015748f920ddc7f8ebe6b21ce2343e3
Guimarret <guimarret@gmail.com>
2025-06-13 23:25:34 -0300

    feat: insert into log file

--- ai-powered-db/src/main.rs
+++ ai-powered-db/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use byteorder::{ByteOrder, LittleEndian};
+
+use tokio::fs::OpenOptions;
+use tokio::io::{self, AsyncWriteExt};
+
+#[tokio::main]
+async fn main(){
+    // #TODO Durability https://calvin.loncaric.us/articles/CreateFile.html
+     let log_insert = OpenOptions::new()
+        .read(true)
+        .create(true)
+        .append(true)
+        .open("foo")
+        .await.unwrap();
+
+    let msg = Message{
+        key: "12312".to_string(),
+        value: "value1231".to_string()
+    };
+
+    publish(vec![msg], log_insert).await;
 }
